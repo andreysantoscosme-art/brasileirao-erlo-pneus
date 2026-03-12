afa52f80dcdac537d0d1b627fb4cc0a84d27a7c4
andreysantoscosme-art <andreysantoscosme@gmail.com>
2026-03-12 19:54:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,121 +1,111 @@
 
-const codes={
-"ERLO1":"Jogador1",
-"ERLO2":"Jogador2",
-"ERLO3":"Jogador3",
-"ERLO4":"Jogador4",
-"ERLO5":"Jogador5",
-"ERLO6":"Jogador6",
-"ERLO7":"Jogador7",
-"ERLO8":"Jogador8",
-"ERLO9":"Jogador9",
-"ERLO10":"Jogador10"
+const select1=document.getElementById("time1")
+const select2=document.getElementById("time2")
+
+TIMES.forEach(t=>{
+let o1=document.createElement("option")
+o1.value=t
+o1.text=t
+select1.appendChild(o1)
+
+let o2=document.createElement("option")
+o2.value=t
+o2.text=t
+select2.appendChild(o2)
+})
+
+let players=[]
+let pontos={}
+
+function teamUsed(t){
+return players.some(p=>p.t1===t||p.t2===t)
 }
 
-let currentUser=null
-let players=JSON.parse(localStorage.getItem("players")||"{}")
-let teamsPoints=JSON.parse(localStorage.getItem("teamsPoints")||"{}")
-let apiKey=localStorage.getItem("apikey")||""
+function addPlayer(){
 
-function login(){
-let code=document.getElementById("code").value
-if(codes[code]){
-currentUser=codes[code]
-document.getElementById("loginScreen").style.display="none"
-document.getElementById("app").style.display="block"
+if(players.length>=10){
+alert("Máximo 10 jogadores")
+return
+}
+
+let nome=document.getElementById("nome").value
+let t1=select1.value
+let t2=select2.value
+
+if(!nome){alert("Digite o nome");return}
+if(t1===t2){alert("Escolha times diferentes");return}
+if(teamUsed(t1)||teamUsed(t2)){alert("Esse time já foi escolhido");return}
+
+players.push({nome:nome,t1:t1,t2:t2})
 render()
-}else{
-alert("Código inválido")
-}
-}
-
-function save(){
-localStorage.setItem("players",JSON.stringify(players))
-localStorage.setItem("teamsPoints",JSON.stringify(teamsPoints))
-render()
-}
-
-function saveTeams(){
-let t1=document.getElementById("team1").value
-let t2=document.getElementById("team2").value
-
-players[currentUser]={team1:t1,team2:t2}
-save()
-}
-
-function updateTeam(){
-let t=document.getElementById("team").value
-let p=parseInt(document.getElementById("points").value)
-teamsPoints[t]=p
-save()
 }
 
 function calc(p){
-return (teamsPoints[p.team1]||0)+(teamsPoints[p.team2]||0)
+return (pontos[p.t1]||0)+(pontos[p.t2]||0)
+}
+
+function removePlayer(i){
+players.splice(i,1)
+render()
 }
 
 function render(){
 
-let ranking=[]
+let ranking=[...players].map(p=>({
+nome:p.nome,
+t1:p.t1,
+t2:p.t2,
+pts:calc(p)
+}))
 
-for(let name in players){
-ranking.push({name:name,points:calc(players[name])})
-}
+ranking.sort((a,b)=>b.pts-a.pts)
 
-ranking.sort((a,b)=>b.points-a.points)
+let tb=document.getElementById("ranking")
+tb.innerHTML=""
 
-let tbody=document.querySelector("#ranking tbody")
-tbody.innerHTML=""
+ranking.forEach((p,i)=>{
 
-ranking.forEach((r,i)=>{
 let tr=document.createElement("tr")
-tr.innerHTML=`<td>${i+1}</td><td>${r.name}</td><td>${r.points}</td>`
-tbody.appendChild(tr)
-})
 
-renderChart(ranking)
-}
+tr.innerHTML=`
+<td>${i+1}</td>
+<td>${p.nome}</td>
+<td>
+<img class="logo" src="${LOGOS[p.t1]}"> ${p.t1} +
+<img class="logo" src="${LOGOS[p.t2]}"> ${p.t2}
+</td>
+<td>${p.pts}</td>
+<td><button onclick="removePlayer(${i})">Excluir</button></td>
+`
 
-let chart
+tb.appendChild(tr)
 
-function renderChart(data){
-
-let labels=data.map(d=>d.name)
-let values=data.map(d=>d.points)
-
-if(chart) chart.destroy()
-
-chart=new Chart(document.getElementById("chart"),{
-type:"line",
-data:{
-labels:labels,
-datasets:[{label:"Pontos",data:values}]
-}
 })
 }
 
-function saveKey(){
-apiKey=document.getElementById("apikey").value
-localStorage.setItem("apikey",apiKey)
-}
+async function updateTable(){
 
-async function updateFromAPI(){
+try{
 
-if(!apiKey){
-alert("Adicione API key")
-return
-}
-
-let res=await fetch("https://api.football-data.org/v4/competitions/BSA/standings",{
-headers:{"X-Auth-Token":apiKey}
-})
-
+let res=await fetch("https://site.api.espn.com/apis/v2/sports/soccer/bra.1/standings")
 let data=await res.json()
 
-data.standings[0].table.forEach(t=>{
-teamsPoints[t.team.name]=t.points
+data.children[0].standings.entries.forEach(t=>{
+
+let name=t.team.displayName
+let pts=t.stats.find(s=>s.name==="points").value
+
+if(name==="RB Bragantino") name="Red Bull Bragantino"
+if(name==="Atlético Mineiro") name="Atlético-MG"
+
+pontos[name]=pts
 })
 
-save()
+render()
+
+}catch(e){}
 
 }
+
+setInterval(updateTable,300000)
+updateTable()

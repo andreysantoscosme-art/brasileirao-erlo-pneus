1d431dfeee04fd8352ffaeadf0262cbe893beed3
andreysantoscosme-art <andreysantoscosme@gmail.com>
2026-03-19 10:43:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
 let jogadores = [];
+let tabela = {};
 
-// TABELA (VOCÊ ATUALIZA COM ESPN)
-const tabela = {
-  "Flamengo": 50,
-  "Palmeiras": 48,
-  "Atlético-MG": 45,
-  "Botafogo": 44,
-  "São Paulo": 42,
-  "Fluminense": 40,
-  "Grêmio": 38,
-  "Internacional": 36,
-  "Athletico-PR": 35,
-  "RB Bragantino": 34,
-  "Cruzeiro": 33,
-  "Vasco da Gama": 30,
-  "Bahia": 29,
-  "Fortaleza": 28,
-  "Santos": 27,
-  "Corinthians": 26,
-  "Goiás": 25,
-  "Coritiba": 22,
-  "América-MG": 20,
-  "Ceará": 18
-};
+async function carregarTimes(){
+  const res = await fetch("https://site.api.espn.com/apis/v2/sports/soccer/bra.1/standings");
+  const data = await res.json();
+
+  const times = data.children[0].standings.entries.map(e=>e.team.displayName);
+
+  const select1 = document.getElementById("time1");
+  const select2 = document.getElementById("time2");
+
+  times.forEach(t=>{
+    const o1 = new Option(t,t);
+    const o2 = new Option(t,t);
+    select1.add(o1);
+    select2.add(o2);
+  });
+}
+
+async function atualizar(){
+  const res = await fetch("https://site.api.espn.com/apis/v2/sports/soccer/bra.1/standings");
+  const data = await res.json();
+
+  tabela = {};
+
+  data.children[0].standings.entries.forEach(e=>{
+    tabela[e.team.displayName] = e.stats.find(s=>s.name==="points").value;
+  });
+
+  jogadores.forEach(j=>{
+    j.pontos = (tabela[j.t1]||0)+(tabela[j.t2]||0);
+  });
+
+  render();
+}
 
 function adicionar(){
   const nome = document.getElementById("nome").value;
   const t1 = document.getElementById("time1").value;
   const t2 = document.getElementById("time2").value;
 
-  if(!nome){
-    alert("Digite o nome");
-    return;
-  }
+  if(!nome){alert("Digite nome");return;}
 
-  jogadores.push({nome, t1, t2, pontos:0});
-  render();
-}
-
-function calcular(){
-  jogadores.forEach(j=>{
-    j.pontos = (tabela[j.t1] || 0) + (tabela[j.t2] || 0);
-  });
+  jogadores.push({nome,t1,t2,pontos:0});
   render();
 }
 
@@ -52,8 +53,10 @@ function render(){
   jogadores.sort((a,b)=>b.pontos-a.pontos);
 
   jogadores.forEach(j=>{
-    const li = document.createElement("li");
-    li.innerText = j.nome + " - " + j.pontos + " pts";
+    const li=document.createElement("li");
+    li.innerText = j.nome+" - "+j.pontos+" pts";
     ul.appendChild(li);
   });
 }
+
+carregarTimes();
